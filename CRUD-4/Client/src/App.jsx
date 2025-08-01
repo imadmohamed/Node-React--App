3119abb5a195ec0176e807e4bbccd23e2cc747c5
imad mohamed <abmohammedimad00@gmail.com>
2025-08-01 17:10:04 +0400

    finish ui design

--- CRUD-4/Client/src/App.jsx
+++ CRUD-4/Client/src/App.jsx
@@ -1,33 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'><h3>CRUD APP front end react and Back end Node.js</h3>
+        <div className='input-search'>
+          <input type='Search' placeholder='Search'/>
+          <button className=' btn green '>Add Record</button>
+        </div>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>S.No</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>City</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Imad</td>
+              <td>22</td>
+              <td>Kalminai</td>
+              <td>
+                <button className='btn green'>Edit</button>
+              </td>
+              <td>
+                <button className='btn red'>Delete</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td>1</td>
+              <td>Imad</td>
+              <td>22</td>
+              <td>Kalminai</td>
+              <td>
+                <button className='btn green'>Edit</button>
+              </td>
+              <td>
+                <button className='btn red'>Delete</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td>1</td>
+              <td>Imad</td>
+              <td>22</td>
+              <td>Kalminai</td>
+              <td>
+                <button className='btn green'>Edit</button>
+              </td>
+              <td>
+                <button className='btn red'>Delete</button>
+              </td>
+            </tr>
+          </tbody>
+
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
