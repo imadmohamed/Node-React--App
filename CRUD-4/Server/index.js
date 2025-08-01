02e05bae94af4ee3d3a45d7a4f8e87e737ee7957
imad mohamed <abmohammedimad00@gmail.com>
2025-08-01 12:41:20 +0400

    create 04th crud

---
+++ CRUD-4/Server/index.js
@@ -0,0 +1,16 @@
+const express = require ("express")
+const users = require("./sample.json")
+
+const app = express()
+const port = 8000;
+
+//Display all users
+
+app.get("/users", (req,res) => {
+    return res.json(users)
+})
+
+
+app.listen(port, (err) => {
+    console.log(`app is runnig in port number ${port}`)
+})
