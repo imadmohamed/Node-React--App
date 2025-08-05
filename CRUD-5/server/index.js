f372ff6f305c7512f8009fe4f9a965243cbe6128
imad mohamed <abmohammedimad00@gmail.com>
2025-08-05 18:09:09 +0400

    ADD some methods and orignin

--- CRUD-5/server/index.js
+++ CRUD-5/server/index.js
@@ -1,8 +1,16 @@
 const express = require ("express");
 const users = require("./sample.json")
+const cors = require("cors");
+
 
 const app = express()
 const port = 8000
+
+app.use(cors({
+    origin:"http://localhost:5173/",
+    methods:"POST, GET, PATCH, DELETE"
+}))
+
 
 //Display all users
 app.get("/users", (req, res)=> {
