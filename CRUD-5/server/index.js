08c8ae688e034ff80846dd8b0d42281f9ee7bc93
imad mohamed <abmohammedimad00@gmail.com>
2025-08-05 19:56:03 +0400

    finsih delete function

--- CRUD-5/server/index.js
+++ CRUD-5/server/index.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const users = require("./sample.json")
 const cors = require("cors");
+const fs = require("fs");
 
 
 const app = express()
@@ -17,6 +18,16 @@ app.get("/users", (req, res)=> {
     return res.json(users)
 })
 
+//Delete user details
+
+app.delete("/users/:id", (req,res)=> {
+    let id = Number(req.params.id);
+    let filterdUsers = users.filter((user)=> user.id !== id)
+    fs.writeFile("./sample.json",JSON.stringify(filterdUsers),(err , data) => {
+        return res.json(filterdUsers)
+    })
+
+})
 
 app.listen(port, (err)=> {
     console.log(`app is runnig is port number ${port}`)
