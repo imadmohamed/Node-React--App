33416e461f444bb5125fe24d2a0eead942643487
imad mohamed <abmohammedimad00@gmail.com>
2025-08-05 18:06:02 +0400

    add fs

---
+++ CRUD-5/client/src/App.jsx
@@ -0,0 +1,63 @@
+import './App.css'
+import axios from "axios"
+import { useState, useEffect } from 'react'
+
+
+function App() {
+
+  const [users, setUser] = useState([])
+  
+  const getAllUsers = async () => {
+    await axios.get("http://localhost:8000/users").then((res) =>{
+      setUser(res.data)
+      console.log(res.data)
+    })
+  }
+
+  useEffect( () => {
+    getAllUsers()
+  },[]);
+
+  return (
+    <>
+      <div className='container'>
+          <h2>CRUD App using React and nodejs</h2>
+          <div className='input-search'>
+            <input type='search'/>
+            <button className='btn green'>Add Record</button>
+            </div>
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th>S.NO</th>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th>City</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {users && users.map((user, index)=> {
+                  return (
+                    <tr key={user.id}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.age}</td>
+                  <td>{user.city}</td>
+                  <td><button className='btn green'>Edit</button></td>
+                  <td><button className='btn red'>Delete</button></td>
+                </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+          
+      
+    </>
+  )
+}
+
+export default App
---
+++ CRUD-5/server/index.js
@@ -0,0 +1,15 @@
+const express = require ("express");
+const users = require("./sample.json")
+
+const app = express()
+const port = 8000
+
+//Display all users
+app.get("/users", (req, res)=> {
+    return res.json(users)
+})
+
+
+app.listen(port, (err)=> {
+    console.log(`app is runnig is port number ${port}`)
+})
